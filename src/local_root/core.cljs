7f17a5597bbf0a11c91e6525afb62d7cc5b37a03
javier abanses <javier.abanses@gmail.com>
2019-01-19 13:29:22 +0000

    antd removed

--- src/local_root/core.cljs
+++ src/local_root/core.cljs
@@ -1,8 +1,6 @@
 (ns local-root.core
-  (:require [reagent.core :as r]
-            [antizer.reagent :as antd]))
+  (:require [reagent.core :as r]))
 
 (defn local-root-resolves []
   [:div
-   [:h1 "component from git-dependency from deps.edn"]
-   [antd/button {:on-click #(js/alert "clicked!")} "click"]])
+   [:h1 "component from git-dependency from deps.edn"]])
